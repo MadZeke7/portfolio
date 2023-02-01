515902fd5dd897f2e31927240ceea3101823a5d4
MadZeke7 <erenji@deloitte.com>
2023-02-01 07:22:46 +0530

    active navbar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,14 +1,40 @@
 import styles from '@/styles/Sidebar.module.css'
 import Link from 'next/link';
+import { useRouter } from "next/router";
+
+const navigationRoutes = ["home", "projects", "interests"];
 
 export default function Sidebar(){
+    const router = useRouter();
     return(
         <div className={styles.sideBar}>
             <ul>
-                <li className={styles.active}>Home</li>
-                <li><Link href="/projects">Projects</Link></li>
-                <li>Interests</li>
+                {navigationRoutes.map((singleRoute, index) => {
+                    return (
+                        <li key={index}>
+                            <NavigationLink
+                                href={`/${singleRoute}`}
+                                text={singleRoute}
+                                router={router}
+                            />
+                        </li>
+                    );
+                })}
             </ul>
         </div>
     );
 }
+
+function NavigationLink({ href, text, router }) {
+    const isActive = router.asPath === (href === "/home" ? "/" : href);
+    return (
+        <Link href={href === "/home" ? "/" : href} className={`${isActive && styles.active}`}>
+            {capitalize(text)}
+        </Link>
+    );
+}
+
+function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+  
